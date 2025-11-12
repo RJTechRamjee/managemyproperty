c3df43c57a47473568bb5916e7bc10ab30eb9e73
Ramjee Korada <koradaramjee@gmail.com>
2025-11-12 18:55:29 +0000

    initial property manager

---
+++ srv/PropertyManager.js
@@ -0,0 +1,21 @@
+const cds = require('@sap/cds')
+
+class PropertyManager {
+
+    constructor() {
+
+    }
+
+    validateProperty() {
+
+    }
+
+    validateNearByAminities(){
+        
+    }
+
+}
+
+module.exports = {
+    PropertyManager
+}
